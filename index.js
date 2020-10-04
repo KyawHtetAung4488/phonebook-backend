0ef55f1ddea13d9bb5884d067b528c1e182f6db5
Kyaw Htet Aung <kyawhtetaung@ucsy.edu.mm>
2020-10-05 00:14:23 +0630

    Complete part3

--- index.js
+++ index.js
@@ -15,28 +15,6 @@ morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
 
 app.use(morgan(':method :url :status :res[content-length] :data - :response-time ms'))
 
-// let persons = [
-//     {
-//       "name": "Arto Hellas",
-//       "number": "040-123456",
-//       "id": 1
-//     },
-//     {
-//       "name": "Ada Lovelace",
-//       "number": "39-44-5323523",
-//       "id": 2
-//     },
-//     {
-//       "name": "Dan Abramov",
-//       "number": "12-43-234345",
-//       "id": 3
-//     },
-//     {
-//       "name": "Mary Poppendieck",
-//       "number": "39-23-6423122",
-//       "id": 4
-//     }
-// ]
 
 // get all persons
 app.get('/api/persons', (req, res) => {
